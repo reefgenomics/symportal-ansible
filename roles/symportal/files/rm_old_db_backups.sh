13fdcb4d1bd8ddda2d635d6ff1938e8c223a830b
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-12-13 16:06:24 +0100

    update backup script

--- roles/symportal/files/rm_old_db_backups.sh
+++ roles/symportal/files/rm_old_db_backups.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Define the directory to start the search
-base_dir="/mnt/prod/db_backups"
+# Log file
+log_file="/mnt/synology_backup.log"
 
-# Find files with names starting with "backup" that are older than 7 days and remove them
-find "$base_dir" -type f -name 'backup-*' -mtime +7 -exec rm -f {} \;
+# Create or append to the log file
+touch "${log_file}"
+
+# Define the directory to start the search
+base_dir="/mnt/diskstation/symportal_machine"
+
+for folder in ${base_dir}/*_backups; do
+    echo "$(date +"%Y-%m-%d %H:%M:%S"): Removing old backups in " ${folder} >> ${log_file}
+    # Find files with names starting with "*.tar.gz" that are older than 3 days and remove them
+    find $folder -type f -name '*.tar.gz' -mtime +3 -exec rm -fv {} \; >> ${log_file}
+done
